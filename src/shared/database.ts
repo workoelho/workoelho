1fe92fe832716875aaee85e8187718f91ec0a2bf
Arthur <arthur@corenzan.com>
2024-02-06 16:11:51 -0300

    Log database operations

--- src/shared/database.ts
+++ src/shared/database.ts
@@ -1,4 +1,19 @@
+import log from "npmlog";
 import { PrismaClient } from "@prisma/client";
 export type * from "@prisma/client";
 
-export const database = new PrismaClient();
+export const database = new PrismaClient().$extends({
+  query: {
+    $allModels: {
+      async $allOperations({ operation, model, args, query }) {
+        performance.mark("request");
+        const result = await query(args);
+        const { duration } = performance.measure("request", "request");
+
+        log.verbose("database", "query", { model, operation, args, duration });
+
+        return result;
+      },
+    },
+  },
+});
