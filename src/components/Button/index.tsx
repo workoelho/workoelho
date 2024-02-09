cc77d278b6722de3bb2d68d835e82c51e178471a
Arthur <arthur@corenzan.com>
2024-02-09 14:29:09 -0300

    Add style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,7 @@
 import { ComponentProps } from "react";
 
+import * as classes from "./style.module.css";
+
 type Props =
   | (ComponentProps<"button"> & {
       method: "get" | "post";
@@ -8,8 +10,11 @@ type Props =
   | ComponentProps<"button">;
 
 export function Button(props: Props) {
+  props.className = classes.button;
+
   if ("action" in props) {
     const { action, method, ...button } = props;
+
     return (
       <form method={method} action={action}>
         <button {...button} />
